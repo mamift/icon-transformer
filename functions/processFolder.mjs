15849dc7a16df40bab606d1921bd94db407e1fda
Sam Haakman <sam@lambagency.com.au>
2019-09-19 14:33:21 +1000

    WB-170 output subfolders

---
+++ functions/processFolder.mjs
@@ -0,0 +1,107 @@
+import fs from "fs";
+import path from "path";
+
+import cheerio from "cheerio";
+import SVGO from "svgo";
+
+import bakeTransforms from "./bakeTransforms";
+import classifySVGAttrs from "./classifySVGAttrs";
+import flattenXlinks from "./flattenXlinks";
+
+export default function processFolder(folder, config, outputSubfolder = null) {
+  const svgo = new SVGO(config.svgo);
+
+  if (outputSubfolder) {
+    fs.mkdirSync(path.resolve(`${config.output}/${outputSubfolder}`), {
+      recursive: true
+    });
+  }
+
+  // FIXME: this is ugly
+  return new Promise(r => {
+    fs.readdir(folder, (e, fileNames) => {
+      r(
+        fileNames.map(
+          async fileName =>
+            new Promise((resolve, reject) => {
+              // Descend into subfolders and run again
+              if (
+                fs
+                  .lstatSync(path.resolve(`${folder}/${fileName}`))
+                  .isDirectory()
+              ) {
+                // FIXME: Total processed number is based on number of Promises returned, but this returns one Promise for potentially many files.
+                return processFolder(
+                  `${folder}/${fileName}`,
+                  config,
+                  fileName
+                ).catch(e => reject(e));
+              }
+              if (!fileName.match(/\.svg$/)) {
+                return reject(
+                  `${fileName} was not processed, please supply only .svg files`
+                );
+              }
+
+              fs.readFile(
+                path.resolve(`${folder}/${fileName}`),
+                "utf8",
+                async (err, icon) => {
+                  const $ = cheerio.load(icon);
+
+                  // TURN USE[XLINK:HREF] INTO REAL ELEMENTS
+                  await flattenXlinks($);
+                  // BAKE TRANSFORMS
+                  if (
+                    config.bakeTransforms === undefined ||
+                    config.bakeTransforms === true
+                  ) {
+                    await bakeTransforms($);
+                  }
+
+                  // CLEAN SPECIFIED ATTRIBUTES
+                  await classifySVGAttrs($, config);
+
+                  // SVGO
+                  const optimised = await svgo
+                    .optimize($("body").html())
+                    .then(({ data }) => data);
+
+                  $("svg").replaceWith(optimised);
+                  $("svg").addClass("icon");
+
+                  const out = $("body").html();
+
+                  // REMOVE FILE PREFIXES
+                  let outName = fileName
+                    .toLowerCase()
+                    .replace(" ", "-")
+                    .replace(
+                      /\.svg$/,
+                      config.outputExtension ? config.outputExtension : ".svg"
+                    );
+                  config.cleanPrefixes.forEach(prefix => {
+                    outName = outName.replace(new RegExp(`^${prefix}`), "");
+                  });
+
+                  // WRITE TO FILE
+                  fs.writeFile(
+                    path.resolve(
+                      `${config.output}/${
+                        outputSubfolder ? outputSubfolder + "/" : ""
+                      }${outName}`
+                    ),
+                    out,
+                    () => resolve(`Saved ${outName}`)
+                  ).catch(e => {
+                    // FIXME: all of these FS operations should probably have a catch/reject
+                    console.warn(e);
+                  });
+                }
+              );
+            })
+        )
+      );
+    });
+  });
+}
--- index.mjs
+++ index.mjs
@@ -4,12 +4,7 @@
 import fs from "fs";
 import path from "path";
 
-import cheerio from "cheerio";
-import SVGO from "svgo";
-
-import bakeTransforms from "./functions/bakeTransforms";
-import classifySVGAttrs from "./functions/classifySVGAttrs";
-import flattenXlinks from "./functions/flattenXlinks";
+import processFolder from "./functions/processFolder";
 
 console.time("Icon processing complete");
 
@@ -17,80 +12,26 @@ const args = process.argv.slice(2);
 
 const config = JSON.parse(fs.readFileSync(path.resolve(args[0]), "utf8"));
 
-const svgo = new SVGO(config.svgo);
-
 // CREATE DIRECTORY FOR ICONS IF IT DOESN'T EXIST
 fs.mkdirSync(path.resolve(config.output), { recursive: true });
 
 let numErrors = 0;
 
-// FIND ALL IN INPUT DIRECTORY
-fs.readdir(config.input, (e, fileNames) => {
+processFolder(config.input, config).then(promises => {
+  let numErrors = 0;
   Promise.all(
-    fileNames.map(async fileName =>
-      new Promise((resolve, reject) => {
-        if (!fileName.match(/\.svg$/)) {
-          return reject(
-            `${fileName} was not processed, please supply only .svg files`
-          );
-        }
-        fs.readFile(
-          path.resolve(`${config.input}/${fileName}`),
-          "utf8",
-          async (err, icon) => {
-            const $ = cheerio.load(icon);
-
-            // TURN USE[XLINK:HREF] INTO REAL ELEMENTS
-            await flattenXlinks($);
-            // BAKE TRANSFORMS
-            if (
-              config.bakeTransforms === undefined ||
-              config.bakeTransforms === true
-            ) {
-              await bakeTransforms($);
-            }
-            // CLEAN SPECIFIED ATTRIBUTES
-            await classifySVGAttrs($, config);
-
-            // SVGO
-            const optimised = await svgo
-              .optimize($("body").html())
-              .then(({ data }) => data);
-
-            $("svg").replaceWith(optimised);
-            $("svg").addClass("icon");
-
-            const out = $("body").html();
-
-            // REMOVE FILE PREFIXES
-            let outName = fileName
-              .toLowerCase()
-              .replace(" ", "-")
-              .replace(
-                /\.svg$/,
-                config.outputExtension ? config.outputExtension : ".svg"
-              );
-            config.cleanPrefixes.forEach(prefix => {
-              outName = outName.replace(new RegExp(`^${prefix}`), "");
-            });
-
-            // WRITE TO FILE
-            fs.writeFile(path.resolve(`${config.output}/${outName}`), out, () =>
-              resolve(`Saved ${outName}`)
-            );
-          }
-        );
-      }).catch(error => {
+    promises.map(p => {
+      p.catch(error => {
         console.warn("\x1b[33m%s\x1b[0m", `⚠️  Notice: ${error}`);
         numErrors += 1;
-      })
-    )
+      });
+    })
   ).then(() => {
     if (numErrors > 0) {
       // Just for an empty line
       console.log();
     }
-    console.log(`${fileNames.length - numErrors} files processed`);
+    console.log(`${promises.length - numErrors} files processed`);
     console.timeEnd("Icon processing complete");
   });
 });
